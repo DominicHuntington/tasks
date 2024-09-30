4ad2cc73725cd9d3a49db2a219e97f9df9cfff84
dominich <dominic.huntington@maine.edu>
2024-09-29 22:56:13 -0400

    did object tasks

--- src/objects.ts
+++ src/objects.ts
@@ -1,121 +1,152 @@
 import { Question, QuestionType } from "./interfaces/question";
 
-/**
- * Create a new blank question with the given `id`, `name`, and `type. The `body` and
- * `expected` should be empty strings, the `options` should be an empty list, the `points`
- * should default to 1, and `published` should default to false.
- */
 export function makeBlankQuestion(
     id: number,
     name: string,
     type: QuestionType
 ): Question {
-    return {};
+    return {
+        id: id,
+        name: name,
+        type: type,
+        body: "",
+        expected: "",
+        options: [],
+        points: 1,
+        published: false
+    };
 }
 
 /**
- * Consumes a question and a potential `answer`, and returns whether or not
- * the `answer` is correct. You should check that the `answer` is equal to
- * the `expected`, ignoring capitalization and trimming any whitespace.
- *
- * HINT: Look up the `trim` and `toLowerCase` functions.
+I have it check if the answer after being trimmed is equal tto the expected. I  have it toUppercase and toLowercase so that
+its capitalization doesnt matter
  */
+
 export function isCorrect(question: Question, answer: string): boolean {
-    return false;
+    return answer.trim().toLowerCase() === question.expected || answer.trim().toUpperCase() === question.expected;
 }
 
 /**
- * Consumes a question and a potential `answer`, and returns whether or not
- * the `answer` is valid (but not necessarily correct). For a `short_answer_question`,
- * any answer is valid. But for a `multiple_choice_question`, the `answer` must
- * be exactly one of the options.
+I have it check what type the question is. if it is short answer then it just goes to valid or true. But if
+its multiple choice i have it check the options to see if the answer is in the options
  */
 export function isValid(question: Question, answer: string): boolean {
+    if(question.type === 'short_answer_question'){
+        return true;
+    }else if (question.type === 'multiple_choice_question'){
+        return question.options.includes(answer);
+    }
     return false;
 }
 
 /**
- * Consumes a question and produces a string representation combining the
- * `id` and first 10 characters of the `name`. The two strings should be
- * separated by ": ". So for example, the question with id 9 and the
- * name "My First Question" would become "9: My First Q".
+I just have it make a new const called short form and have that be set to a substring made up of spaces 0-10
  */
 export function toShortForm(question: Question): string {
-    return "";
+    const shortform = question.name.substring(0,10);
+    return `${question.id}: ${shortform}`; // prints it formatted
 }
 
 /**
- * Consumes a question and returns a formatted string representation as follows:
- *  - The first line should be a hash sign, a space, and then the `name`
- *  - The second line should be the `body`
- *  - If the question is a `multiple_choice_question`, then the following lines
- *      need to show each option on its line, preceded by a dash and space.
- *
- * The example below might help, but don't include the border!
- * ----------Example-------------
- * |# Name                      |
- * |The body goes here!         |
- * |- Option 1                  |
- * |- Option 2                  |
- * |- Option 3                  |
- * ------------------------------
- * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    let markdown = `# ${question.name}\n${question.body}`;// formats it to be name then newline then body
+    if (question.type === 'multiple_choice_question'){
+        question.options.forEach(option => { // goes through each option that is stored in question.options and to be added to markdown for printing
+            markdown += `\n- ${option}`; // makes sure they are newlined and printed formatted
+        });
+    }
+    return markdown;
+
 }
 
 /**
- * Return a new version of the given question, except the name should now be
- * `newName`.
+ I did it this way so that it has all of the info copied but didnt mutate the original and instead made
+ a new question
  */
 export function renameQuestion(question: Question, newName: string): Question {
-    return question;
+    return {
+        id: question.id,
+        name: newName, 
+        type: question.type,
+        body: question.body,
+        expected: question.expected,
+        options: question.options,
+        points: question.points,
+        published: question.published,
+    };
 }
 
 /**
- * Return a new version of the given question, except the `published` field
- * should be inverted. If the question was not published, now it should be
- * published; if it was published, now it should be not published.
+Like the last one I made a new question that copied the olds info but then I used ! to invert whatever
+the original questions published to be for the new copy
  */
 export function publishQuestion(question: Question): Question {
-    return question;
+    return {
+        id: question.id,
+        name: question.name, 
+        type: question.type,
+        body: question.body,
+        expected: question.expected,
+        options: question.options,
+        points: question.points,
+        published: !question.published,
+    };
 }
 
 /**
- * Create a new question based on the old question, copying over its `body`, `type`,
- * `options`, `expected`, and `points` without changes. The `name` should be copied
- * over as "Copy of ORIGINAL NAME" (e.g., so "Question 1" would become "Copy of Question 1").
- * The `published` field should be reset to false.
+ I just have it make a new one with all the infro from the original except I have it change the name
+ to be the string "copy of" added to the oldquestion.name
  */
 export function duplicateQuestion(id: number, oldQuestion: Question): Question {
-    return oldQuestion;
+        return{
+        id: id,
+        name: "Copy of " + oldQuestion.name, 
+        type: oldQuestion.type,
+        body: oldQuestion.body,
+        expected: oldQuestion.expected,
+        options: oldQuestion.options,
+        points: oldQuestion.points,
+        published: false,
+        };
 }
 
 /**
- * Return a new version of the given question, with the `newOption` added to
- * the list of existing `options`. Remember that the new Question MUST have
- * its own separate copy of the `options` list, rather than the same reference
- * to the original question's list!
- * Check out the subsection about "Nested Fields" for more information.
+I just have it make a new one using all of the info from the original except I have it
+add the given addOption to the list that is in question.options
  */
 export function addOption(question: Question, newOption: string): Question {
-    return question;
+    return {
+        id: question.id,
+        name: question.name, 
+        type: question.type,
+        body: question.body,
+        expected: question.expected,
+        options: [...question.options,newOption], 
+        points: question.points,
+        published: question.published,
+    };
 }
 
 /**
- * Consumes an id, name, and two questions, and produces a new question.
- * The new question will use the `body`, `type`, `options`, and `expected` of the
- * `contentQuestion`. The second question will provide the `points`.
- * The `published` status should be set to false.
- * Notice that the second Question is provided as just an object with a `points`
- * field; but the function call would be the same as if it were a `Question` type!
+
  */
+
 export function mergeQuestion(
     id: number,
     name: string,
     contentQuestion: Question,
     { points }: { points: number }
+
 ): Question {
-    return contentQuestion;
+    return {
+        id,                        // sets the given id
+        name,                      // sets the given name
+        body: contentQuestion.body, // sets the body from contentQuestion
+        type: contentQuestion.type, // sets the type from contentQuestion
+        options: contentQuestion.options, // sets the options from contentQuestion
+        expected: contentQuestion.expected, // sets the expected from contentQuestion
+        points,                   // sets the points from the second question
+        published: false          // sets published to be false
+    };
 }
